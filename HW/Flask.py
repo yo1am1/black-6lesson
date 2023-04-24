134c175123b2f52d687fc92ff3616bef07abd817
Yo1 <bigdiebam@gmail.com>
2023-04-25 00:24:16 +0300

    Homework

--- HW/Flask.py
+++ HW/Flask.py
@@ -1,55 +1,22 @@
 import sqlite3
 
 import flask
-from faker import Faker
 from flask import Flask
 
-fake = Faker()
-
-
-try:
-    con = sqlite3.connect('music.db')
-    c = con.cursor()
-    c.execute('''CREATE TABLE customers
-                 (id INTEGER,
-                  first_name TEXT,
-                  last_name TEXT,
-                  email TEXT)''')
-
-    for i in range(200):
-        first_name = fake.first_name()
-        last_name = fake.last_name()
-        email = fake.email()
-        c.execute(f"INSERT INTO customers (id, first_name, last_name, email) VALUES ({i}, '{first_name}', '{last_name}', '{email}')")
-
-    c.execute('''CREATE TABLE tracks
-                 (id INTEGER,
-                  title TEXT,
-                  artist TEXT,
-                  len_in_sec INTEGER)''')
-
-    for i in range(200):
-        title = fake.text(40)
-        artist = fake.name()
-        len_in_sec = fake.random_int(min=90, max=240)
-        c.execute(f"INSERT INTO tracks (id, title, artist, len_in_sec) VALUES ({i}, '{title}', '{artist}', {len_in_sec})")
-
-    con.commit()
-    con.close()
-except (Exception,):
-    pass
-
+from HW import data_create
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    data_create.create()
     return flask.render_template("index.html")
 
 
 @app.route('/names/')
 def unique_names():
+    data_create.create()
     con = sqlite3.connect('music.db')
     c = con.cursor()
     names = c.execute("SELECT COUNT(DISTINCT first_name) FROM customers").fetchone()[0]
@@ -59,6 +26,7 @@ def unique_names():
 
 @app.route('/tracks/')
 def track_count():
+    data_create.create()
     con = sqlite3.connect('music.db')
     c = con.cursor()
     track = c.execute("SELECT COUNT(*) FROM tracks").fetchone()[0]
@@ -68,6 +36,7 @@ def track_count():
 
 @app.route('/tracks-sec/')
 def track_lengths():
+    data_create.create()
     con = sqlite3.connect('music.db')
     c = con.cursor()
     results = c.execute("SELECT title, len_in_sec FROM tracks").fetchall()
---
+++ HW/data_create.py
@@ -0,0 +1,45 @@
+import sqlite3
+import random
+from faker import Faker
+
+
+
+def create():
+    try:
+        fake = Faker()
+        con = sqlite3.connect('music.db')
+        c = con.cursor()
+        c.execute('''CREATE TABLE customers
+                     (id INTEGER,
+                      first_name TEXT,
+                      last_name TEXT,
+                      email TEXT)''')
+
+        for i in range(random.randint(100, 400)):
+            first_name = fake.first_name()
+            last_name = fake.last_name()
+            email = fake.email()
+            c.execute(
+                f"INSERT INTO customers (id, first_name, last_name, email) VALUES ({i}, '{first_name}', '{last_name}', '{email}')")
+
+        c.execute('''CREATE TABLE tracks
+                     (id INTEGER,
+                      title TEXT,
+                      artist TEXT,
+                      len_in_sec INTEGER)''')
+
+        for i in range(random.randint(100, 250)):
+            title = fake.text(40)
+            artist = fake.name()
+            len_in_sec = fake.random_int(min=90, max=240)
+            c.execute(
+                f"INSERT INTO tracks (id, title, artist, len_in_sec) VALUES ({i}, '{title}', '{artist}', {len_in_sec})")
+
+        con.commit()
+        con.close()
+    except (Exception,):
+        pass
+
+
+if __name__ == '__main__':
+    create()
